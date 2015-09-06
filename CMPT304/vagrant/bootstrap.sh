88c5ab7c57dbc94ebf3157aa80c38ba78185731e
Tyler Kuipers <tkuipers123@gmail.com>
2015-09-06 16:01:16 -0600

    Added files containing simple vagrant setup for CMPT304

---
+++ CMPT304/vagrant/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+#install apache
+apt-get update
+# apt-get install apache2
+apt-get install -y tomcat7
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant /var/www
+fi
+
+#install Java
+apt-get install -y openjdk-7-jdk
